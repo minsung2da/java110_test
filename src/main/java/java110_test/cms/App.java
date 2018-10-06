7255d6cb6967fb81ae1ef75f5a5e240ef5722e12
minsung2da <minsung2da@naver.com>
2018-10-06 17:39:46 +0900

    패키지 변경

--- src/main/java/java110_test/cms/App.java
+++ src/main/java/java110_test/cms/App.java
@@ -1,36 +1,10 @@
+package java110_test.cms;
+
 import java.util.Scanner;
 
 public class App {
 
-    static class Member{
-        
-        String name;
-        String age;
-        String email;
-        
-        
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-        public String getAge() {
-            return age;
-        }
-        public void setAge(String age) {
-            this.age = age;
-        }
-        public String getEmail() {
-            return email;
-        }
-        public void setEmail(String email) {
-            this.email = email;
-        }
 
-    }
-    
-    
     static Member [] members = new Member[100];
     
     static Scanner scan = new Scanner(System.in);
---
+++ src/main/java/java110_test/cms/Member.java
@@ -0,0 +1,29 @@
+package java110_test.cms;
+
+public class Member {
+
+    String name;
+    String age;
+    String email;
+    
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
